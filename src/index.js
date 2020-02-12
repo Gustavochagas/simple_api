5b9779f6a883e6fb66fa0acbfe26164afc979778
gustavochagass <gustavochagasb@gmail.com>
2020-02-12 15:57:51 -0200

    version

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const routes = require("./routes");
+const env = require("../env");
+
+const app = express();
+
+mongoose.connect(
+  `mongodb+srv://${env.USER_MONGO}:${env.PASSWORD_MONGO}@cluster0-ozbpa.mongodb.net/week10?retryWrites=true&w=majority`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  }
+);
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
