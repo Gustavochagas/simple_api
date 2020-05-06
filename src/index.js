57098ef7b581971ff2089f4456677b79d03ae375
gustavochagass <gustavochagasb@gmail.com>
2020-05-06 19:44:27 -0300

    socket

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const http = require('http');
 const routes = require("./routes");
 const env = require("../env");
+const { setupWebSocket } = require('./websocket');
 
 const app = express();
+const server = http.Server(app);
+
+setupWebSocket(server);
 
 mongoose.connect(
   `mongodb+srv://${env.USER_MONGO}:${env.PASSWORD_MONGO}@cluster0-ozbpa.mongodb.net/week10?retryWrites=true&w=majority`,
@@ -19,4 +24,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
